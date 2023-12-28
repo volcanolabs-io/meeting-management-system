ce90f1e5f3eb744bda2fc51ca0f9edaf8dea61bd
John McPeek <john.mcpeek@volcanolabs.io>
2023-12-28 14:35:59 -0500

    Initial Commit.

---
+++ src/main/java/io/volcanolabs/medium/meetingmanagementsystem/MeetingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package io.volcanolabs.medium.meetingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( MeetingManagementSystemApplication.class, args );
+	}
+
+}
---
+++ src/test/java/io/volcanolabs/medium/meetingmanagementsystem/MeetingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.volcanolabs.medium.meetingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
